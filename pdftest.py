6a5b77e2b8c91c293b62f51f4845bb0c4087a947
glazor14 <lazorcgd@dukes.jmu.edu>
2018-08-21 10:19:39 -0400

    Updated parsing method to account for changes

--- pdftest.py
+++ pdftest.py
@@ -5,23 +5,27 @@ from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.tokenize import MWETokenizer
 import nltk
-nltk.download('punkt')
-nltk.download('stopwords')
+#nltk.download('punkt')
+#nltk.download('stopwords')
 
 def SOW_Parsing_TaM(text_file):
+	"""
+	This method will return the parsed data from the text file in the proper format.
+	@param text-file text variable which contains all the text in pdf file.
+	@returns dictionary of values
+	"""
 	#The first step is to read text from the file
 	file = open(text_file, "r")
 	text = file.read()
 	print(text)
 
-	"""
-	We have a text variable which contains all the text in pdf file.
-	"""
+	pp = pprint.PrettyPrinter(indent=4)
+
 	#The word_tokenize() function will break our text phrases into #individual words
 	tokens = word_tokenize(text)
 
 	#we'll create a new list which contains punctuation we wish to clean
-	punctuations = ['(',')',';',':','[',']',',','.']
+	punctuations = ['(',')',';',':','[',']',',','.', '\"']
 
 	#stop_words = stopwords.words('english')
 
@@ -34,6 +38,7 @@ def SOW_Parsing_TaM(text_file):
 
 	get_indexes = lambda x, xs: [i for (y, i) in zip(xs, range(len(xs))) if x == y]
 
+	pp.pprint(keywords)
 	# Find the Title of the Document
 	pindexes = get_indexes("Project", keywords)
 	thindexes = get_indexes("This", keywords)
@@ -42,22 +47,25 @@ def SOW_Parsing_TaM(text_file):
 		if keywords[i+1] == "Title":
 			break
 		a = a +1
-	#print (a)
 	projectTitle = (keywords[pindexes[a]+2:thindexes[0]])
 	projectTitle = ' '.join(projectTitle)
 	print ("Project Title: ", projectTitle)
 
 	# Find the Client Name
-	capindexes = get_indexes("ﬁCapTechﬂ", keywords)
-	cindexes = get_indexes("ﬁClientﬂ", keywords)
+	capindexes = get_indexes("CapTech", keywords)
+	cindexes = get_indexes("Client", keywords)
 
-	clientName = keywords[capindexes[0]+2:cindexes[0]]
+	#print (a)
+	#print (capindexes)
+	#print (cindexes)
+
+	clientName = keywords[capindexes[0]+7:cindexes[0]-1]
 	clientName = ' '.join(clientName)
 	print("Client Name: ", clientName)
 
 	# Find the Client Contact Info
 	cciIndexes = get_indexes("Client", keywords)
-	cpindexes = get_indexes("CapTech", keywords)
+	cpindexes = get_indexes("Inc.", keywords)
 	a = 0
 	for i in cciIndexes:
 		if keywords[i+1] == "Contact" and keywords[i+2] == "Information":
@@ -66,18 +74,21 @@ def SOW_Parsing_TaM(text_file):
 
 	b = 0
 	for i in cpindexes:
-		if keywords[i+1] == "Contact" and keywords[i+2] == "Information":
+		if keywords[i+1] == "Keith" and keywords[i+2] == "Smith":
 			break
 		b = b + 1
 
-	clientContactInfo = keywords[cciIndexes[a] + 3:cpindexes[b]]
+	#print("a", a, "b", b)
+	#print(cciIndexes)
+	#print(cpindexes)
+	clientContactInfo = keywords[cciIndexes[a]+6:cpindexes[b]-2]
 	clientContactInfo = ' '.join(clientContactInfo)
 	print ("Client Contact Info: ", clientContactInfo)
 
 	# Find the Captech Contact Info
 	conindexes = get_indexes("captechconsulting.com", keywords)
 
-	captechContactInfo = keywords[cpindexes[b]:conindexes[0]+1]
+	captechContactInfo = keywords[cpindexes[b]+1:conindexes[0]+1]
 	captechContactInfo = ' '.join(captechContactInfo)
 	print("Captech Contact Info: ", captechContactInfo)
 
@@ -89,24 +100,27 @@ def SOW_Parsing_TaM(text_file):
 	a = 0
 	c = 0
 
+	#print (rindexes)
+	#print (sindexes)
+
 	for i in rindexes:
-		if keywords[i+1] == "Responsibilities":
+		if keywords[i+3] == "Responsibilities" or keywords[i+2] == "Responsibilities" or keywords[i+4] == "Responsibilities":
 			break
 		a = a +1
 
 	for i in sindexes:
-		if keywords[i-1] == "III":
+		if keywords[i+1] == "III":
 			break
 		c = c +1
 
-	#print (a)
-	servicesAndDevs = keywords[rindexes[a]:sindexes[c]-1]
+	#print ("a", a, "c", c)
+	servicesAndDevs = keywords[rindexes[a]:sindexes[c]]
 	servicesAndDevs = ' '.join(servicesAndDevs)
 	print ("Services and Deliverables: ", servicesAndDevs)
 
 	# Find Dates
 	bindexes = get_indexes("beginning", keywords)
-	eindexes = get_indexes("end", keywords)
+	eindexes = get_indexes("ending", keywords)
 
 	a = 0
 	d = 0
@@ -116,7 +130,7 @@ def SOW_Parsing_TaM(text_file):
 		a = a +1
 
 	for i in eindexes:
-		if keywords[i+1] == "ing":
+		if keywords[i+1] == "on":
 			break
 		d = d +1
 
@@ -124,7 +138,7 @@ def SOW_Parsing_TaM(text_file):
 	#print(bindexes)
 
 	begDates = keywords[bindexes[a]+2:bindexes[a]+5]
-	endDates = keywords[eindexes[d]+3:eindexes[d]+6]
+	endDates = keywords[eindexes[d]+2:eindexes[d]+5]
 
 	print("Start Date: ", ' '.join(begDates))
 	print("End Date: ", ' '.join(endDates))
@@ -136,22 +150,58 @@ def SOW_Parsing_TaM(text_file):
 
 	# Find the Payment table
 
-	rindexes = get_indexes("Role", keywords)
-	tindexes = get_indexes("Total", keywords)
+	rindexes = get_indexes("Payment", keywords)
+	tindexes = get_indexes("Rate", keywords)
+	eindexes = get_indexes("Estimated", keywords)
+	totindexes = get_indexes("Total", keywords)
 
 	#print(rindexes)
 	#print(tindexes)
 
 	a = 0
+	b = 0
+	h = 0
+	c = 0
+	f = 0
 
 	for i in rindexes:
-		if keywords[i+1] == "Rate":
+		if keywords[i+1] == "Role":
 			break
 		a = a +1
 
+	for i in tindexes:
+		if keywords[i+1] == '$':
+			break
+		b = b + 1
+
+	for i in eindexes:
+		if keywords[i+1] == 'Hours':
+			break
+		h = h + 1
+
+	for i in eindexes:
+		if keywords[i+1] == 'Cost':
+			break
+		c = c + 1
+
+	for i in totindexes:
+		if keywords[i+2] == '$':
+			break
+		f = f + 1
+
 	#print (a)
+	role = keywords[rindexes[a] + 2:tindexes[b]]
+	eHours = keywords[eindexes[h] + 2:eindexes[c]]
+	eCost = keywords[eindexes[c]+ 2:totindexes[f]]
+	total = keywords[totindexes[f]+1: totindexes[f]+4]
+
 	paymentTable = ' '.join(keywords[rindexes[a]:tindexes[0]+5])
 
-	print ("Payment Table: ", paymentTable)
+	print("Role: ", ' '.join(role))
+	print("Estimated Hours: ", ' '.join(eHours))
+	print("Estimated Cost: ", ' '.join(eCost))
+	print("Total: ", ' '.join(total))
+
+	#print ("Payment Table: ", paymentTable)
 
 
